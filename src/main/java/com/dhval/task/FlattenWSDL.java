711f9fbb5aab0f0cf1afc821695b5966e45b7c77
Dhval Mudawal <dhval.m@gmail.com>
2017-09-17 23:20:25 -0400

    refactored transfoem method signature

--- src/main/java/com/dhval/task/FlattenWSDL.java
+++ src/main/java/com/dhval/task/FlattenWSDL.java
@@ -9,6 +9,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
+import javax.xml.transform.stream.StreamSource;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
@@ -21,16 +22,20 @@ public class FlattenWSDL {
     private static final String XSL_SORT_NS = "xsl/sort-schema.xsl";
     private static final String XSL_FLATTEN = "xsl/flatten-wsdl.xsl";
 
-    public static void flatten(String wsdlFile) throws SaxonApiException, IOException, URISyntaxException {
-        String outFile = FileUtils.addFileExtension(wsdlFile, "_Full");
-        List<String> list = new FindSchemaLocations().buildFromWsdl(wsdlFile);
+    public static void flatten(String wsdlLocation) throws SaxonApiException, IOException, URISyntaxException {
+        if (!FileUtils.isFilePresent(wsdlLocation)) {
+            throw new IllegalArgumentException("Not found, WSDL -" + wsdlLocation);
+        }
+        String outLocation = FileUtils.addFileExtension(wsdlLocation, "_Full");
+        File outFile = new File(outLocation);
+        List<String> list = new FindSchemaLocations().buildFromWsdl(wsdlLocation);
         List<String> xsdFiles = new FindSchemaLocations().buildFromXsd(list);
         LOG.info(("Found (xsd)# " + xsdFiles.size()));
         Path basePath = Paths.get(new ClassPathResource(XSL_SORT_NS).getFile().getAbsolutePath()).getParent();
         File schemas = basePath.resolveSibling("schemas.xml").toFile();
 
         new XMLWriter().buildSchemas(schemas, xsdFiles.toArray(new String[xsdFiles.size()]));
-        new TransformUtils().transform(XSL_FLATTEN, wsdlFile, outFile);
-        new TransformUtils().transform(XSL_SORT_NS, outFile, outFile);
+        new TransformUtils().transform(new ClassPathResource(XSL_FLATTEN).getFile(), new File(wsdlLocation), outFile);
+        new TransformUtils().transform(new ClassPathResource(XSL_SORT_NS).getFile(), outFile, outFile);
     }
 }
--- src/main/java/com/dhval/utils/TransformUtils.java
+++ src/main/java/com/dhval/utils/TransformUtils.java
@@ -1,6 +1,9 @@
 package com.dhval.utils;
 
+import com.dhval.task.FlattenWSDL;
 import net.sf.saxon.s9api.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.io.ClassPathResource;
 
 import javax.xml.transform.stream.StreamSource;
@@ -9,19 +12,17 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 public class TransformUtils {
-
-    public void transform(String xslFile, String xmlFile, String outFile) throws SaxonApiException, IOException, URISyntaxException {
-        if (!FileUtils.isFilePresent(xmlFile)) {
-            throw new IllegalArgumentException("Not found, xmlFile -" + xmlFile);
-        }
+    private static final Logger LOG = LoggerFactory.getLogger(TransformUtils.class);
+    public void transform(File xslFile, File xmlFile, File outFile) throws SaxonApiException, IOException, URISyntaxException {
+        LOG.info("Transform(" + xslFile.getName() + ", " + xmlFile.getName() + ") > " + outFile.getName());
         Processor proc = new Processor(false);
         XsltCompiler comp = proc.newXsltCompiler();
-        XsltExecutable exp = comp.compile(new StreamSource(new ClassPathResource(xslFile).getFile()));
-        XdmNode source = proc.newDocumentBuilder().build(new StreamSource(new File(xmlFile)));
+        XsltExecutable exp = comp.compile(new StreamSource(xslFile));
+        XdmNode source = proc.newDocumentBuilder().build(new StreamSource(xmlFile));
         Serializer out = new Serializer();
         out.setOutputProperty(Serializer.Property.METHOD, "xml");
         out.setOutputProperty(Serializer.Property.INDENT, "yes");
-        out.setOutputFile(new File(outFile));
+        out.setOutputFile(outFile);
         XsltTransformer trans = exp.load();
         trans.setInitialContextNode(source);
         trans.setDestination(out);
